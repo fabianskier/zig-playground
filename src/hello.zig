2bdc5d4e60f42b9f6eb401b3137df75c959b62bc
fabianskier <fabianskier@icloud.com>
2023-04-15 18:59:19 +0100

    CO-0002: Init project with `zig init-exe`

--- src/hello.zig
+++ src/hello.zig
@@ -1,10 +1,10 @@
 const std = @import("std");
 
-fn greet() !void {
+pub fn world() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Hello, {s}!\n", .{"world"});
 }
 
 test "greet" {
-    try greet();
+    try world();
 }
